af5209d54798ab9708750ba671743d1987f7cfea
Adam Lezotte <adam@lezotte.me>
2017-03-11 09:49:09 -0500

    index, two routes, and controller

---
+++ script.js
@@ -0,0 +1,21 @@
+var app = angular.module("coolApp", ["ngRoute"]);
+
+app.config(function($routeProvider, $locationProvider) {
+    
+    $routeProvider
+        .when("/sayhello", {
+            controller: "coolController",
+            templateUrl: "sayhello.html"
+        })
+        .when("/saygoodbye", {
+            controller: "coolController",
+            templateUrl: "saygoodbye.html"
+        })
+        .otherwise({redirectTo: "/"});
+
+        $locationProvider.hashPrefix("");
+});
+
+app.controller("coolController", function($scope) {
+
+});
